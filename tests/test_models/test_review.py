22d99d0b0340264d5ac0c49afccbd3c5f4eaf79f
Lovelynpearl <lovelynpearl@gmail.com>
2023-02-14 07:11:25 -0800

    Add all tasks

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,11 @@
+ unittests for models/review.py.
+
+ Unittest classes:
+
+         TestReview_instantiation
+
+             TestReview_save
+
+                 TestReview_to_dict
+
+                 """
